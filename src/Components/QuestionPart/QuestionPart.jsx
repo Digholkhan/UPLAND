543b312b3d69ccde0cd14982292c24642a09ff90
Digholkhan <Digholkhan@gmail.com>
2024-06-27 17:51:24 +0600

    modified

--- src/Components/QuestionPart/QuestionPart.jsx
+++ src/Components/QuestionPart/QuestionPart.jsx
@@ -8,6 +8,8 @@ import Heading from '../Heading';
 import { FaPlus } from "react-icons/fa6";
 import { FaMinus } from "react-icons/fa6";
 import BgElement from '../../assets/BgElement.png'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const QuestionPart = () => {
   const [show,setShow]= useState(false);
@@ -30,7 +32,7 @@ const QuestionPart = () => {
                     </div>
                 </div>
               <div>
-                <div className='pt-20 lg:pt-0 z-10'>
+                <div className='pt-20 lg:pt-0 z-10' >
                   <Image Imgsrc={dots}/>
                   <Heading className='pt-3 pb-12'>Frequently Asked Questions</Heading>
                 </div>
@@ -48,7 +50,8 @@ const QuestionPart = () => {
                             </div>
                           }
                         </div>
-                        <button onClick={()=>setShow(!show)} className={`absolute top-2 right-0 ${show?"text-red-500":"text-secondary"}`}>{show?<FaMinus/>:<FaPlus/>}</button>
+                        {/* <button onClick={()=>setShow(!show)} className={`absolute top-2 right-0 ${show?"text-red-500":"text-secondary"}`}>{show?<FaMinus/>:<FaPlus/>}</button> */}
+                        <FaMinus className={`absolute top-2 right-0 transition-all duration-300 ${show?"opacity-100":"opacity-0 -rotate-45"}`} /><FaPlus className={`absolute top-2 right-0 transition-all duration-300 ${show?"opacity-0 rotate-45":"opacity-100"}`} />
                     </div>
                     <div className={`relative flex items-start justify-between cursor-pointer  ${show1?'pb-0':'pb-5'}`} onClick={()=>setShow1(!show1)}> 
                     <div className='w-[555px]'>
@@ -64,8 +67,7 @@ const QuestionPart = () => {
                       </div>
                       <button onClick={()=>setShow1(!show1)} className={`absolute top-2 right-0 ${show1?"text-red-500":"text-secondary"}`} >{show1?<FaMinus />:<FaPlus />}</button>
                     </div>
-
-                  <div className={`relative flex items-start justify-between cursor-pointer  ${show2?'pb-0':'pb-5'}`} onClick={()=>setShow2(!show2)}> 
+                    <div className={`relative flex items-start justify-between cursor-pointer  ${show2?'pb-0':'pb-5'}`} onClick={()=>setShow2(!show2)}> 
                     <div className='w-[555px]'>
                         <h3 className={`text-xl lg:text-2xl font-medium border-b-2 border-dotted  pb-5 ${show2?"border-red-500":"border-secondary"}`}>Can I use free plan permanently?
                         </h3>
@@ -78,7 +80,7 @@ const QuestionPart = () => {
                         }  
                     </div>
                       <button onClick={()=>setShow2(!show2)} className={`absolute top-2 right-0 ${show2?"text-red-500":"text-secondary"}`}>{show2?<FaMinus />:<FaPlus />}</button>
-                  </div>         
+                    </div>         
                 </div>
               </div>
             </Flex>
